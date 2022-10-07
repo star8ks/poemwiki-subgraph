40cee7c60b4515092d37bb32683aa768fa138877
Ray Victor <ray7551@gmail.com>
2022-10-08 06:41:15 +0800

    init

---
+++ src/governor.ts
@@ -0,0 +1,131 @@
+import {
+  ContractInitialized as ContractInitializedEvent,
+  ProposalCanceled as ProposalCanceledEvent,
+  ProposalCreated as ProposalCreatedEvent,
+  ProposalExecuted as ProposalExecutedEvent,
+  ProposalThresholdSet as ProposalThresholdSetEvent,
+  QuorumNumeratorUpdated as QuorumNumeratorUpdatedEvent,
+  VoteCast as VoteCastEvent,
+  VoteCastWithParams as VoteCastWithParamsEvent,
+  VotingDelaySet as VotingDelaySetEvent,
+  VotingPeriodSet as VotingPeriodSetEvent
+} from "../generated/Contract/Contract"
+import {
+  ContractInitialized,
+  ProposalCanceled,
+  ProposalCreated,
+  ProposalExecuted,
+  ProposalThresholdSet,
+  QuorumNumeratorUpdated,
+  VoteCast,
+  VoteCastWithParams,
+  VotingDelaySet,
+  VotingPeriodSet
+} from "../generated/schema"
+
+export function handleContractInitialized(
+  event: ContractInitializedEvent
+): void {
+  let entity = new ContractInitialized(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.version = event.params.version
+  entity.save()
+}
+
+export function handleProposalCanceled(event: ProposalCanceledEvent): void {
+  let entity = new ProposalCanceled(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.proposalId = event.params.proposalId
+  entity.save()
+}
+
+export function handleProposalCreated(event: ProposalCreatedEvent): void {
+  let entity = new ProposalCreated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.proposalId = event.params.proposalId
+  entity.proposer = event.params.proposer
+  entity.targets = event.params.targets
+  entity.values = event.params.values
+  entity.signatures = event.params.signatures
+  entity.calldatas = event.params.calldatas
+  entity.startBlock = event.params.startBlock
+  entity.endBlock = event.params.endBlock
+  entity.description = event.params.description
+  entity.save()
+}
+
+export function handleProposalExecuted(event: ProposalExecutedEvent): void {
+  let entity = new ProposalExecuted(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.proposalId = event.params.proposalId
+  entity.save()
+}
+
+export function handleProposalThresholdSet(
+  event: ProposalThresholdSetEvent
+): void {
+  let entity = new ProposalThresholdSet(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.oldProposalThreshold = event.params.oldProposalThreshold
+  entity.newProposalThreshold = event.params.newProposalThreshold
+  entity.save()
+}
+
+export function handleQuorumNumeratorUpdated(
+  event: QuorumNumeratorUpdatedEvent
+): void {
+  let entity = new QuorumNumeratorUpdated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.oldQuorumNumerator = event.params.oldQuorumNumerator
+  entity.newQuorumNumerator = event.params.newQuorumNumerator
+  entity.save()
+}
+
+export function handleVoteCast(event: VoteCastEvent): void {
+  let entity = new VoteCast(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.voter = event.params.voter
+  entity.proposalId = event.params.proposalId
+  entity.support = event.params.support
+  entity.weight = event.params.weight
+  entity.reason = event.params.reason
+  entity.save()
+}
+
+export function handleVoteCastWithParams(event: VoteCastWithParamsEvent): void {
+  let entity = new VoteCastWithParams(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.voter = event.params.voter
+  entity.proposalId = event.params.proposalId
+  entity.support = event.params.support
+  entity.weight = event.params.weight
+  entity.reason = event.params.reason
+  entity.params = event.params.params
+  entity.save()
+}
+
+export function handleVotingDelaySet(event: VotingDelaySetEvent): void {
+  let entity = new VotingDelaySet(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.oldVotingDelay = event.params.oldVotingDelay
+  entity.newVotingDelay = event.params.newVotingDelay
+  entity.save()
+}
+
+export function handleVotingPeriodSet(event: VotingPeriodSetEvent): void {
+  let entity = new VotingPeriodSet(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.oldVotingPeriod = event.params.oldVotingPeriod
+  entity.newVotingPeriod = event.params.newVotingPeriod
+  entity.save()
+}
